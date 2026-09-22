7b9d50ed3f5567892fb420195793b3b867f42036
Mark Lester <marklestermarananpal@gmail.com>
2026-09-22 23:05:53 +0800

    Update dependencies and refactor application structure. Added react-router-dom for routing, removed unused App.css and index.css files, and modified App component to use AppRoutes with BrowserRouter for improved navigation.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { Provider } from "./components/ui/provider"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import AppRoutes from './routes/AppRoutes.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>  
-      <App />
+    <Provider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
